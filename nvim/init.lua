491f0bb2c1b96d93e815946edee0ee9aecf7b996
Kenedy <kenedy@kenedy.dev>
2025-03-25 19:00:47 -0300

    refactor(nvim): restructure config with lazy.nvim and optimized keymaps
    - replace manual plugin system with lazy.nvim
    - add bootstrap logic for lazy.nvim
    - reorganize general settings and key mappings
    - add shortcuts for navigation, wrapping, diagnostics, and clipboard
    - define custom signs and highlight colors for diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,31 @@
--- General settings
+-- # bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 1 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(2)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- # leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- # general settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
-vim.g.mapleader = " "
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
@@ -15,174 +35,93 @@ vim.opt.scrolloff = 4
 vim.opt.wrap = false
 vim.opt.fixendofline = true
 
-local plugins = {} -- Table tracking plugins to install and manage
-local install_path = vim.fn.stdpath("config") .. "/pack/plugins/start/" -- Plugin installation directory
+-- # key mappings
+-- select all text with ctrl-a
+vim.keymap.set({ "i", "n", "v" }, "<C-a>", "<Esc>ggVG", { noremap = true, silent = true })
 
--- Function to install or update plugins
-local function update_plugins()
-    print("Updating Neovim plugins...")
+-- copy to system clipboard
+vim.keymap.set("v", "<C-c>", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<C-c>", '"+yy', { noremap = true, silent = true })
 
-    for repository, _ in pairs(plugins) do
-        local plugin_name = repository:match(".*/(.-)%.git$")
-        local plugin_path = install_path .. plugin_name
-
-        if vim.fn.isdirectory(plugin_path) == 0 then
-            print("Cloning " .. plugin_name .. "...")
-            vim.fn.system("git clone --depth=1 " .. repository .. " " .. plugin_path)
-        else
-            print("Updating " .. plugin_name .. "...")
-            vim.fn.system("git -C " .. plugin_path .. " pull --ff-only")
-        end
-    end
-
-    print("Plugin update complete! Restart Neovim if necessary.")
-end
-
--- Create the command to update plugins manually
-vim.api.nvim_create_user_command("UpdatePlugins", update_plugins, {})
-
--- Function to safely require a plugin
-local function safe_require(module_name)
-    local ok, module = pcall(require, module_name)
-    return ok and module or nil
-end
-
--- Helper function to encapsulate plugin configuration
-local function use(module, opts)
-    opts = opts or {}
-
-    if opts.repository then
-        plugins[opts.repository] = true
-    end
-
-    if opts.dependencies then
-        for _, dep in ipairs(opts.dependencies) do
-            plugins[dep] = true
-        end
-    end
-
-    local plugin = safe_require(module)
-    if plugin and opts.config then
-        opts.config(plugin)
-    else
-        vim.schedule(function()
-            vim.notify("Could not load: " .. module, vim.log.levels.WARN)
-        end)
-    end
-end
-
--- Plugin configurations
-use("tokyonight", {
-    repository = "https://github.com/folke/tokyonight.nvim.git",
-    config = function(tokyonight)
-        tokyonight.setup({ style = "night" })
-        tokyonight.load()
-    end
-})
-
-use("bufferline", {
-    repository = "https://github.com/akinsho/bufferline.nvim.git",
-    dependencies = { "https://github.com/nvim-tree/nvim-web-devicons.git" },
-    config = function(bufferline)
-        bufferline.setup({ options = { separator_style = "slant", always_show_bufferline = false } })
-    end
-})
-
-use("lualine", {
-    repository = "https://github.com/nvim-lualine/lualine.nvim.git",
-    dependencies = { "https://github.com/nvim-tree/nvim-web-devicons.git" },
-    config = function(lualine)
-        lualine.setup({})
-    end
-})
-
-use("nvim-treesitter.configs", {
-    repository = "https://github.com/nvim-treesitter/nvim-treesitter.git",
-    config = function(treesitter)
-        treesitter.setup({
-            ensure_installed = { "ruby", "python", "html", "css", "javascript", "c", "cpp", "lua", "markdown", "markdown_inline" },
-            highlight = { enable = true },
-            auto_install = true,
-            indent = { enable = false },
-        })
-    end
-})
-
-use("Comment", {
-    repository = "https://github.com/numToStr/Comment.nvim.git",
-    config = function(comment)
-        comment.setup({})
-    end
-})
-
-use("mini.pairs", {
-    repository = "https://github.com/echasnovski/mini.pairs.git",
-    config = function(mini_pairs)
-        mini_pairs.setup({})
-    end
-})
-
-use("ibl", {
-    repository = "https://github.com/lukas-reineke/indent-blankline.nvim.git",
-    config = function(ibl)
-        ibl.setup({ indent = { char = "│", tab_char = "│" }, scope = { show_start = false, show_end = false } })
-    end
-})
-
-use("which-key", {
-    repository = "https://github.com/folke/which-key.nvim.git",
-    dependencies = {
-        "https://github.com/echasnovski/mini.icons.git",
-        "https://github.com/nvim-tree/nvim-web-devicons.git"
-    },
-    config = function(which_key)
-        which_key.setup({ preset = "helix" })
-    end
-})
-
-use("nvim-surround", {
-    repository = "https://github.com/kylechui/nvim-surround.git",
-    dependencies = {
-        "https://github.com/nvim-treesitter/nvim-treesitter.git",
-        "https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git"
-    },
-    config = function(nvim_surround)
-        nvim_surround.setup()
-    end
-})
-
--- Key mappings
-vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
-vim.keymap.set("v", "<C-c>", "\"+y", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-c>", "\"+yy", { noremap = true, silent = true })
+-- save file with ctrl-s
 vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
 
--- Toggle relative numbers
-vim.keymap.set("n", "<Leader>rn", function()
-    vim.opt.relativenumber = not vim.opt.relativenumber:get()
+-- toggle relative numbers
+vim.keymap.set("n", "<Leader>Tr", function()
+	vim.opt.relativenumber = not vim.opt.relativenumber:get()
 end, { noremap = true, silent = true, desc = "Toggle relative numbers" })
 
--- BufferLine shortcuts
-if safe_require("bufferline") then
-  vim.keymap.set("n", "<Leader>bh", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true, desc = "Go to previous buffer" })
-  vim.keymap.set("n", "<Leader>bl", ":BufferLineCycleNext<CR>", { noremap = true, silent = true, desc = "Go to next buffer" })
-  vim.keymap.set("n", "<Leader>bco", ":BufferLineCloseOthers<CR>", { noremap = true, silent = true, desc = "Close all other buffers" })
-  vim.keymap.set("n", "<Leader>bcr", ":BufferLineCloseRight<CR>", { noremap = true, silent = true, desc = "Close buffers to the right" })
-  vim.keymap.set("n", "<Leader>bcl", ":BufferLineCloseLeft<CR>", { noremap = true, silent = true, desc = "Close buffers to the left" })
-  vim.keymap.set("n", "<Leader>b]", ":BufferLineMoveNext<CR>", { noremap = true, silent = true, desc = "Move buffer to the right" })
-  vim.keymap.set("n", "<Leader>b[", ":BufferLineMovePrev<CR>", { noremap = true, silent = true, desc = "Move buffer to the left" })
+-- window navigation shortcuts
+vim.keymap.set("n", "<Leader>wh", "<C-w>h", { noremap = true, silent = true, desc = "Move to left window" })
+vim.keymap.set("n", "<Leader>wj", "<C-w>j", { noremap = true, silent = true, desc = "Move to lower window" })
+vim.keymap.set("n", "<Leader>wk", "<C-w>k", { noremap = true, silent = true, desc = "Move to upper window" })
+vim.keymap.set("n", "<Leader>wl", "<C-w>l", { noremap = true, silent = true, desc = "Move to right window" })
+
+-- clear search
+vim.keymap.set("n", "<Esc>", ":noh<CR>", { noremap = true, silent = true, desc = "Clear search highlights" })
+
+-- toggle line wrapping
+vim.keymap.set("n", "<Leader>Tw", function()
+	vim.opt.wrap = not vim.opt.wrap:get()
+end, { noremap = true, silent = true, desc = "Toggle line wrap" })
+
+-- diagnostic key mappings
+-- stylua: ignore start
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true, desc = "Go to previous diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { noremap = true, silent = true, desc = "Go to next diagnostic" })
+vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Show diagnostic float" })
+vim.keymap.set("n", "<Leader>q", vim.diagnostic.setloclist, { noremap = true, silent = true, desc = "Set location list with diagnostics" })
+vim.keymap.set("n", "<Leader>Td", function()
+  if vim.diagnostic.is_disabled(0) then
+    vim.diagnostic.enable(0)
+  else
+    vim.diagnostic.disable(0)
+  end
+end, { noremap = true, silent = true, desc = "Toggle diagnostics for current buffer" })
+-- stylua: ignore end
+
+-- # setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+	},
+	rocks = {
+		hererocks = false,
+	},
+	-- colorscheme that will be used when installing plugins
+	install = { colorscheme = { "tokyodark" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+-- # diagnostic signs & configuration
+
+-- define diagnostic signs
+local diagnostic_signs = {
+	Error = " ",
+	Warn = " ",
+	Hint = " ",
+	Info = "",
+}
+
+for type, icon in pairs(diagnostic_signs) do
+	vim.fn.sign_define("DiagnosticSign" .. type, { text = icon, texthl = "DiagnosticSign" .. type })
 end
-vim.keymap.set("n", "<Leader>bcc", ":bd<CR>", { noremap = true, silent = true, desc = "Close current buffer" })
-vim.keymap.set("n", "<Leader>bd", ":bd!<CR>", { noremap = true, silent = true, desc = "Force close current buffer" })
 
--- Window shortcuts
-vim.keymap.set("n", "<Leader>wh", "<C-w>h", { noremap = true, silent = true, desc = "Move to the window on the left" })
-vim.keymap.set("n", "<Leader>wj", "<C-w>j", { noremap = true, silent = true, desc = "Move to the window below" })
-vim.keymap.set("n", "<Leader>wk", "<C-w>k", { noremap = true, silent = true, desc = "Move to the window above" })
-vim.keymap.set("n", "<Leader>wl", "<C-w>l", { noremap = true, silent = true, desc = "Move to the window on the right" })
+-- configure diagnostics
+vim.diagnostic.config({
+	virtual_text = { prefix = "●", spacing = 2 },
+	signs = true,
+	underline = false,
+	severity_sort = true,
+	float = { border = "rounded", source = "always" },
+})
 
--- Toggle wrap
-vim.keymap.set("n", "<leader>lw", function()
-    vim.opt.wrap = not vim.opt.wrap:get()
-end, { noremap = true, silent = true, desc = "Toggle wrap" })
+-- custom diagnostic highlight colors
+vim.cmd([[
+  highlight DiagnosticError guifg=#F44747 gui=italic
+  highlight DiagnosticWarn  guifg=#FF8800 gui=italic
+  highlight DiagnosticInfo  guifg=#4FC1FF gui=italic
+  highlight DiagnosticHint  guifg=#D7BA7D gui=italic
+]])

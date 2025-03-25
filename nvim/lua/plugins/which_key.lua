16d56e1dd87e9d270900d5f2ede22fae9932f699
Kenedy <kenedy@kenedy.dev>
2025-03-25 20:47:50 -0300

    feat(nvim): add which-key.nvim plugin for keybinding discovery
    - enables which-key with `helix` preset for consistent layout
    - includes support for icons via mini.icons and web-devicons
    - loads plugin lazily on VeryLazy event

---
+++ nvim/lua/plugins/which_key.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/which-key.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"echasnovski/mini.icons",
+	},
+	event = "VeryLazy",
+	opts = {
+		preset = "helix",
+	},
+}

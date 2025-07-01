001eae0cab3cd77b2cac7e32e9301e9528bcf406
Kenedy <kenedy@kenedy.dev>
2025-07-01 11:36:32 -0300

    design(nvim): disable plugin update checker in lazy.nvim setup
    - comments out the checker.enabled option to prevent automatic plugin update checks

--- nvim/init.lua
+++ nvim/init.lua
@@ -92,7 +92,7 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins
 	install = { colorscheme = { "tokyodark" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	-- checker = { enabled = true },
 })
 
 -- # diagnostic signs & configuration
